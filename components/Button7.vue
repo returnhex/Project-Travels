3621a6fcb9eb64160e82a0ad39e18838826d21a6
Service2Returnhex <service2.returnhex@gmail.com>
2025-07-02 00:36:58 +0600

    remove Button7  calc height
    remove Button7 calc height

--- components/Button7.vue
+++ components/Button7.vue
@@ -6,7 +6,7 @@ defineProps({
 <template>
   <!-- btn2 -->
   <button
-    class="group relative inline-flex h-[calc(45px+8px)] items-center justify-center rounded-full bg-whitegap-2 px-2 py-1 md:px-3 md:py-2 border border-red text-red tex-sm md:text-lg xl:text-[1rem] font-semibold cursor-pointer hover:text-white"
+    class="group relative inline-flex h-[53px] items-center justify-center rounded-full bg-whitegap-2 px-2 py-1 md:px-3 md:py-2 border border-red text-red tex-sm md:text-lg xl:text-[1rem] font-semibold cursor-pointer hover:text-white"
   >
     <span class="z-10 pr-12 font-[500] text-md md:text-lg">{{ title }}</span>
     <div
